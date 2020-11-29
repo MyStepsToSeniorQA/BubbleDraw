a39bfac4c2c9f52a502a9013e921e5ef169f8143
Mikhail <MyStepsToSeniorQA@gmail.com>
2020-11-29 11:21:03 +0300

    onTouch, speed update, motion event

--- app/src/main/java/com/example/bubbledraw/BubbleView.java
+++ app/src/main/java/com/example/bubbledraw/BubbleView.java
@@ -27,13 +27,24 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
     public BubbleView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         bubbleList = new ArrayList<Bubble>();
-        testBubbles();
+        //testBubbles();
+        setOnTouchListener(this);
 
     }
+    private Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            for(Bubble b: bubbleList)
+                b.update();
+            invalidate();
+
+        }
+    };
 
     protected void onDraw(Canvas canvas) {
         for(Bubble b: bubbleList)
             b.draw(canvas);
+        h.postDelayed(r, delay);
 
     }
 
@@ -48,10 +59,16 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return false;
-    }
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+        for (int n = 0; n < motionEvent.getPointerCount(); n++) {
+            int x = (int) motionEvent.getX(n);
+            int y = (int) motionEvent.getY(n);
+            int s = rand.nextInt(size) + size;
+            bubbleList.add(new Bubble(x, y, s));
+        }
+            return true;
 
+    }
 
     private class Bubble {
         private int x;
@@ -59,7 +76,7 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
         private int size;
         private int color;
         private int xspeed, yspeed;
-        private final int MAX_SPEED = 5;
+        private final int MAX_SPEED = 30;
 
         public Bubble(int newX, int newY, int newSize) {
             x = (newX / newSize) * newSize + newSize / 2;
@@ -69,9 +86,10 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
                     rand.nextInt(256),
                     rand.nextInt(256),
                     rand.nextInt(256));
-            xspeed = yspeed = 2;
+            xspeed = rand.nextInt(MAX_SPEED * 2 + 1) - MAX_SPEED;
+            yspeed = rand.nextInt(MAX_SPEED * 2 + 1) - MAX_SPEED;
 
-            if (xspeed == 0 & yspeed == 0) xspeed = yspeed = 1;
+            if (xspeed == 0 & yspeed == 0) xspeed = yspeed = 2;
 
         }
 
